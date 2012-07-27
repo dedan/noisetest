5318c29717a2b3fa9a86a68ddb4d020ee463f445
Stephan Gabler <stephan.gabler@gmail.com>
2012-07-27 11:07:41 +0200

    first time good settings for good detection --> many fals positives (what I want)

--- face_detect.py
+++ face_detect.py
@@ -15,7 +15,7 @@ from scipy import signal
 
 image_size = 100
 kernel_size = 5
-iterations = 1
+iterations = 1000
 
 kernel = np.ones((kernel_size, kernel_size))
 
@@ -27,19 +27,19 @@ for i in range(iterations):
 
     t = cv.GetTickCount()
     rand_im = np.random.rand(image_size, image_size)
-    im = signal.convolve(rand_im, kernel)
+    im = signal.convolve(rand_im, kernel, mode='valid')
 
     im = ((im / np.max(im)) * 255).astype(np.uint8)
     img = cv.fromarray(im)
 
     noise_creation_times.append(cv.GetTickCount() - t)
 
-    # xml_file = 'haarcascade_frontalface_default.xml'
-    xml_file = 'haarcascade_eye.xml'
-    xml_file = 'haarcascade_frontalface_alt.xml'
+    xml_file = 'haarcascade_frontalface_default.xml'
+    # xml_file = 'haarcascade_eye.xml'
+    # xml_file = 'haarcascade_frontalface_alt.xml'
     hc = cv.Load('/usr/local/share/opencv/haarcascades/' + xml_file)
     faces = cv.HaarDetectObjects(img, hc, cv.CreateMemStorage(), 1.2, 1,
-                                 cv.CV_HAAR_DO_CANNY_PRUNING)
+                                 cv.CV_HAAR_DO_CANNY_PRUNING, (30, 30))
     detection_times.append(cv.GetTickCount() - t)
 
     for (x,y,w,h),n in faces:
