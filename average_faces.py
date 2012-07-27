fix bug in patch averaging (least frequent patch gave nan)

average_faces.py
@@ -10,7 +10,8 @@ import pylab as plt
 
 res_folder = '/Users/dedan/projects/bci/noisetest/res2'
 
-n_images = 3
+n_images = 15
+max_size = 50
 
 # compute histogram
 res = pickle.load(open(os.path.join(res_folder, 'res.pckl')))
@@ -20,7 +21,7 @@ counts, bins = np.histogram(widths, range=(minw, maxw), bins=(maxw-minw))
 
 indeces = list(reversed(np.argsort(counts.tolist()).tolist()))
 
-for i, idx in enumerate(indeces):
+for i, idx in enumerate(indeces[:n_images]):
 
     patches = [r for r in res if r.shape[0] == int(bins[idx])]
     patches_mean = np.mean(np.array(patches), axis=0)