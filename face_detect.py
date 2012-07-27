9486a5be7f674a2a67a66cb20deca13519f4f9fc
Stephan Gabler <stephan.gabler@gmail.com>
2012-07-27 11:42:51 +0200

    store patch size distribution

--- face_detect.py
+++ face_detect.py
@@ -15,12 +15,14 @@ from scipy import signal
 
 image_size = 100
 kernel_size = 5
-iterations = 1000
+iterations = 10000
 
 kernel = np.ones((kernel_size, kernel_size))
 
 noise_creation_times = []
 detection_times = []
+sizes = []
+res = []
 img = cv.LoadImage('lena.jpeg', 0)
 
 for i in range(iterations):
@@ -39,11 +41,14 @@ for i in range(iterations):
     # xml_file = 'haarcascade_frontalface_alt.xml'
     hc = cv.Load('/usr/local/share/opencv/haarcascades/' + xml_file)
     faces = cv.HaarDetectObjects(img, hc, cv.CreateMemStorage(), 1.2, 1,
-                                 cv.CV_HAAR_DO_CANNY_PRUNING, (30, 30))
+                                 cv.CV_HAAR_DO_CANNY_PRUNING)
     detection_times.append(cv.GetTickCount() - t)
 
     for (x,y,w,h),n in faces:
+        # TODO: save the detected patches
         cv.Rectangle(img, (x,y), (x+w,y+h), 255)
+        sizes.append((w, h))
+        res.append()
     if faces:
         cv.SaveImage("faces_detected_%d.jpg" % i, img)
 
@@ -51,3 +56,13 @@ avg_noise_time = np.mean(noise_creation_times)
 print "avg, noise creation time = %gms" % (avg_noise_time/(cv.GetTickFrequency()*1000.))
 avg_detection_time = np.mean(detection_times)
 print "avg, detection_times time = %gms" % (avg_detection_time/(cv.GetTickFrequency()*1000.))
+
+plt.figure()
+plt.subplot(211)
+plt.hist([x[0] for x in sizes])
+plt.subplot(212)
+plt.hist([x[1] for x in sizes])
+plt.savefig('sizes_hist.png')
+
+# TODO: average the patches (1: only some of equal size, 2: interpolate to equal size)
+
